e46e02b836e7ff0da8d8889f73dbb2ad54a95dd0
waleed7333 <waleed733150@gmail.com>
2024-09-08 09:14:10 +0300

    update js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,66 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
-  }
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data));
+  }, []);
+
+  const addToy = (newToy) => {
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((response) => response.json())
+      .then((addedToy) => setToys((prevToys) => [...prevToys, addedToy]));
+  };
+
+  const deleteToy = (id) => {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    }).then(() =>
+      setToys((prevToys) => prevToys.filter((toy) => toy.id !== id))
+    );
+  };
+
+  const likeToy = (id, likes) => {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes }),
+    })
+      .then((response) => response.json())
+      .then((updatedToy) => {
+        setToys((prevToys) =>
+          prevToys.map((toy) => (toy.id === id ? updatedToy : toy))
+        );
+      });
+  };
+
+  const handleClick = () => {
+    setShowForm((prevShowForm) => !prevShowForm);
+  };
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} deleteToy={deleteToy} likeToy={likeToy} />
     </>
   );
 }
